67bc5fd4bbe648a85a11595abaf033cc24c03e05
Rodrigo Pupe <rodrigopupe@outlook.com>
2020-03-23 19:35:34 -0300

    * Included a new database class: "WordRoomDatabase". * Change the package of the class "WordDao".

--- app/src/main/java/br/com/rmp/wordlist/database/WordDao.kt
+++ app/src/main/java/br/com/rmp/wordlist/database/WordDao.kt
@@ -1,5 +1,6 @@
-package br.com.rmp.wordlist.dao
+package br.com.rmp.wordlist.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
@@ -9,7 +10,7 @@ import br.com.rmp.wordlist.entity.Word
 @Dao
 interface WordDao {
     @Query("SELECT * FROM word_table ORDER BY word ASC")
-    fun getWordsSortedAlphabetically(): List<Word>
+    fun getWordsSortedAlphabetically(): LiveData<List<Word>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertWord(word: String)
